6ac82dea1610892fbd65a9b2a637422ac5e33da3
John Ferguson <JAFerguson952@gmail.com>
2024-09-12 21:29:14 +0100

    added tracing

--- activate.zsh
+++ activate.zsh
@@ -1,2 +1,5 @@
 #!/bin/zsh
 export RUSTDOCFLAGS="--html-in-header $(pwd)/docs/html/custom-header.html --document-private-items"
+export CARGO_FEATURE_VIEWER=1
+export CARGO_FEATURE_ENABLE_TRACE=1
+export TOPO_LOG=all=info
