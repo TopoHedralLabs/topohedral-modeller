5eb3efb8dddc51cc6dda3c30eb37cf7eedc01d1c
John Ferguson <JAFerguson952@gmail.com>
2024-09-12 21:29:29 +0100

    added folds

--- src/boxing/box_bcurve.rs
+++ src/boxing/box_bcurve.rs
@@ -8,26 +8,28 @@ use topohedral_optimisation::scalar::{minimize_scalar,
     MinimizeScalarOptions, MinimizeScalarReturns, Method, Bounds};
 
 
-impl<const D: usize> ABoxable for Bcurve<D>
+//{{{ impl<const D: usize> Bcurve<D>
+impl<const D: usize> Bcurve<D>
 where
     [(); D + 1]:,
     [(); D * BCURVE_DER_MAX]:,
     [(); D * 3]:,
     [(); D * 2]:,
 {
+
     fn compute_box(&mut self)
     {
+        //{{{ locals
         let mut mins = [std::f64::MAX; D];
         let mut mins_idx = [0; D];
         let mut maxs = [std::f64::MIN; D];
         let mut max_idx = [0; D];
-        let mut box_vals = [0.0; D * 2];
-
-
+        let mut min_vals = [0.0; D];
+        let mut max_vals = [0.0; D];
         let p = self.p();
         let knots = self.knots();
-
-        // find the min and max values at knots in each dimension
+        //}}}
+        //{{{ com: find the min and max values at knots in each dimension
         let n = knots.len();
         let start = p;
         let end = n - p;
@@ -49,19 +51,19 @@ where
                 }
             }
         }
-
-        // create minimisation options struct, initialise it to use the bounded 1D method
+        //}}}
+        //{{{ com: create minimisation options struct, initialise it to use the bounded 1D method
         let mut min_scal_opts = MinimizeScalarOptions {
             method:  Method::Bounded, 
             bounds: Bounds::Pair((0.0, 0.0)),
             tol: 1e-8, 
             max_iter: 100,
         };
-
-        // For each dimension, bracket the min/max and then perform the minimisation.
+        //}}}
+        //{{{ com: For each dimension, bracket the min/max and then perform the minimisation. 
         for j in 0..D 
         {
-            // start with the minimum value for dim j
+            //{{{ com: min for dimension j
             let min_idx = mins_idx[j];
             let min_interval = match min_idx
             {
@@ -84,11 +86,9 @@ where
             };
 
             let min_res = minimize_scalar(fmin, &min_scal_opts).unwrap();
-            box_vals[2 * j] = min_res.fmin;
-
-
-
-            // move onto maximum value for dim j
+            min_vals[j] = min_res.fmin;
+            //}}}
+            //{{{ com: max for dimension j
             let max_idx = max_idx[j];
             let max_interval = match max_idx
             {
@@ -112,27 +112,26 @@ where
 
             let max_res = minimize_scalar(fmax, &min_scal_opts).unwrap();
 
-            box_vals[(2 * j)+1] = -max_res.fmin;
-
+            max_vals[j] = -max_res.fmin;
+            //}}}
         }
-
-        // Finally, assign to the abox field:
-        match D {
-            2 => {
-                self.abox = Some(ABox::new_2d(box_vals[0], box_vals[1], 
-                                              box_vals[2], box_vals[3]));
-            }
-            3 => {
-
-                self.abox = Some(ABox::new_3d(box_vals[0], box_vals[1], 
-                                              box_vals[2], box_vals[3], 
-                                              box_vals[4], box_vals[5]));
-            }
-            _ => panic!("D must be 2 or 3"),    
-        }
+        //}}}
+        //{{{ com: Finally, assign to the abox field:
+        self.abox = Some(ABox::new(min_vals, max_vals));
+        //}}}
     }
+}
+//}}}
+//{{{ impl<const D: usize> ABoxable<D> for Bcurve<D>
+impl<const D: usize> ABoxable<D> for Bcurve<D>
+where
+    [(); D + 1]:,
+    [(); D * BCURVE_DER_MAX]:,
+    [(); D * 3]:,
+    [(); D * 2]:,
+{
 
-    fn get_box(&mut self) -> &ABox
+    fn get_box(&mut self) -> &ABox<D>
     {
         if self.abox.is_none() 
         {
@@ -142,12 +141,15 @@ where
     }
 }
 //..................................................................................................
+//}}}
 
 
 //-------------------------------------------------------------------------------------------------
+//{{{ mod: tests
 #[cfg(test)]
 mod tests
 {
+  
     use super::*;
     use approx::{assert_abs_diff_eq, assert_relative_eq, ulps_eq, AbsDiff};
 
@@ -163,9 +165,9 @@ mod tests
         bcurve.compute_box();
 
 
-        let abox1: ABox = ABox::new_3d( 3.5943976280809996e-7, 9.931728335174615, 
-                                       -5.7727909628567495, 0.46365419036864053, 
-                                       -1.9466863224019781, 1.9533790139511857);
+        let mins = [3.5943976280809996e-7,-5.7727909628567495, -1.9466863224019781];
+        let maxs = [9.931728335174615, 0.46365419036864053, 1.9533790139511857];
+        let abox1: ABox<3> = ABox::<3>::new(mins, maxs);
         let abox2 = bcurve.get_box();
 
         assert_relative_eq!(abox1.xmin(), abox2.xmin(), epsilon = 1e-9);
@@ -178,3 +180,4 @@ mod tests
     }
     //..............................................................................................
 }
+//}}}
