45e5d7aea12dff6b76d6b0708d1f2d98738e9726
John Ferguson <JAFerguson952@gmail.com>
2024-09-12 21:29:42 +0100

    changed the design

--- src/boxing/common.rs
+++ src/boxing/common.rs
@@ -1,90 +1,113 @@
-//! This submodule contains the common types, functions, constants, enums and traits which are 
-//! shared across this module
+//! Short Description of module
 //!
+//! Longer description of module
 //--------------------------------------------------------------------------------------------------
 
+//{{{ crate imports 
+use crate::common::Vector;
+//}}}
+//{{{ std imports 
+//}}}
+//{{{ dep imports 
+//}}}
+//--------------------------------------------------------------------------------------------------
 
+//{{{ struct: ABox<const D: usize> 
 #[derive(Debug, Clone)]
-pub struct ABox
+pub struct ABox<const D: usize>
 {
-    dims: [f64; 6]
+    min: [f64; D], 
+    max: [f64; D],
 }
 //..................................................................................................
-
-impl ABox
+//}}}
+//{{{ impl<const D: usize>  ABox<D>
+impl<const D: usize>  ABox<D>
 {
-    pub fn new_2d(x1: f64, x2: f64, y1: f64, y2: f64) -> Self {
-
-        ABox {
-            dims: [x1, x2, y1, y2, f64::NAN, f64::NAN]
+    //{{{ fun: new
+    pub fn new(min: [f64; D], max: [f64; D]) -> Self
+    {
+        Self {
+            min: min,
+            max: max,
         }
     }
-    pub fn new_3d(x1: f64, x2: f64, y1: f64, y2: f64, z1: f64, z2: f64) -> Self {
-        ABox {
-            dims: [x1, x2, y1, y2, z1, z2]
-        }
-    }
-    pub fn xmin(&self) -> f64 { self.dims[0] }
-    pub fn xmax(&self) -> f64 { self.dims[1] }
-    pub fn ymin(&self) -> f64 { self.dims[2] }
-    pub fn ymax(&self) -> f64 { self.dims[3] }
-    pub fn zmin(&self) -> f64 
-    { 
-        if self.is_3d()
-        {
-            self.dims[4] 
-        }
-        else {
-            0.0
-        }
-    }
-    pub fn zmax(&self) -> f64 
-    { 
-        if self.is_3d()
-        {
-            self.dims[5]
-        }
-        else {
-            0.0
-        }
-    }
-    pub fn is_2d(&self) -> bool {
-        self.dims[4].is_nan() && self.dims[5].is_nan()
-    }
-    pub fn is_3d(&self) -> bool {
-        !self.dims[4].is_nan() && !self.dims[5].is_nan()
-    }
-
+    //}}}
+    //{{{ fun min
+    pub fn min(&self, i: usize) -> f64 { self.min[i] }  
+    //}}}
+    //{{{ fun max
+    pub fn max(&self, i: usize) -> f64 { self.max[i] }  
+    //}}}
+    //{{{ fun length
+    pub fn length(&self, i: usize) -> f64{ self.max[i] - self.min[i] }    
+    //}}}
+    //{{{ fun: diameter
     pub fn diameter(&self) -> f64 {
-        let dx = self.xmax() - self.xmin();
-        let dy = self.ymax() - self.ymin();
-        let dz = self.zmax() - self.zmin();
-        (dx * dx + dy * dy + dz * dz).sqrt()
+        let mut diam = 0.0f64;
+        for i in 0..D {
+            diam += (self.max[i] - self.min[i]).powi(2);
+        }
+        diam.sqrt() 
     }
-
+    //}}}
+    //{{{ fun: measure
+    pub fn measure(&self) -> f64 { 
+        let mut meas = 1.0f64;
+        for i in 0..D {
+            meas *= (self.max[i] - self.min[i]);
+        }
+        meas
+    }
+    //}}}
+    //{{{ fun: origin
+    pub fn origin(&self) -> Vector<D> {
+        let mut origin = Vector::<D>::zeros();
+        origin.copy_from_slice(&self.min);
+        origin
+    }
+    //}}}
+    //{{{ fun: center
+    fn center(&self) -> Vector<D>
+    {
+        let mut center = Vector::<D>::zeros();
+        for i in 0..D {
+            center[i] = (self.max[i] + self.min[i]) / 2.0;
+        }
+        center
+    }
+    //}}}
 }
 //..................................................................................................
+//}}}
+//{{{ impl ABox<2>
+impl ABox<2>
+{
 
-impl From<[f64; 4]> for ABox {
-    fn from(arr: [f64; 4]) -> Self {
-        ABox::new_2d(arr[0], arr[1], arr[2], arr[3])
-    }
-}   
-//..................................................................................................
-
-impl From<[f64; 6]> for ABox {
-    fn from(arr: [f64; 6]) -> Self {
-        ABox::new_3d(arr[0], arr[1], arr[2], arr[3], arr[4], arr[5])
-    }
+    pub fn xmin(&self) -> f64 { self.min[0] }
+    pub fn xmax(&self) -> f64 { self.max[0] }
+    pub fn ymin(&self) -> f64 { self.min[1] }
+    pub fn ymax(&self) -> f64 { self.max[1] }
 }
-//..................................................................................................
+//}}}
+//{{{ impl ABox<3>
+impl ABox<3> 
+{
 
+    pub fn xmin(&self) -> f64 { self.min[0] }
+    pub fn xmax(&self) -> f64 { self.max[0] }
+    pub fn ymin(&self) -> f64 { self.min[1] }
+    pub fn ymax(&self) -> f64 { self.max[1] }
+    pub fn zmin(&self) -> f64 { self.min[2] }
+    pub fn zmax(&self) -> f64 { self.max[2] }
+}
+//}}}
+//{{{ impl ABoxable
 /// This trait defines boxable types. Meaning types with a presence in 2D or 3D space for which 
 /// the limits of their extent can be computed and stored in a bounding box.
 /// 
 /// Types that implement this trait are expected to have a ``Option<Box>`` field that is 
 /// lazily evaluated
-pub trait ABoxable {
-    fn compute_box(&mut self);
-    fn get_box(&mut self) -> &ABox;
-}
+pub trait ABoxable<const D: usize> {
+    fn get_box(&mut self) -> &ABox<D>;
+}//}}}
