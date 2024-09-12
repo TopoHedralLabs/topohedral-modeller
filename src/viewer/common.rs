e1f9b5fd25b0a9208f0a1f4ff38814785530a4e6
John Ferguson <JAFerguson952@gmail.com>
2024-09-12 21:32:25 +0100

    Single Viewable trait for both 2D and 3D, single Convert trait for 2D and 3D

--- src/viewer/common.rs
+++ src/viewer/common.rs
@@ -1,28 +1,43 @@
+//! Common definitions for the viewer submodule
+//!
+//!
+//--------------------------------------------------------------------------------------------------
 
+//{{{ crate imports 
+use crate::common::{Vec2, Vec3, Vector};
+//}}}
+//{{{ std imports 
+use std::fmt::Debug;
+//}}}
+//{{{ dep imports 
+pub use topohedral_viewer as tv;
+use topohedral_viewer::Color;
+//}}}
+//--------------------------------------------------------------------------------------------------
 
-
-
-use topohedral_viewer::{Color, d2, d3};
-
-use crate::common::{Vec2, Vec3};
-pub use topohedral_viewer::Vec3 as Vec3f32;
-
-pub trait Vecf64ToVecf32 {
-    fn convert(self) -> Vec3f32;
+pub trait Convert<const D: usize> 
+{
+    fn convert(&self) -> tv::VecD<D>;
 }
 
-impl Vecf64ToVecf32 for Vec2 {
-    fn convert(self) -> Vec3f32 {
-        Vec3f32::new(self.x as f32, self.y as f32, 0.0f32)
+impl Convert<2> for Vec2
+{
+    fn convert(&self) -> tv::VecD<2>
+    {
+        tv::VecD::<2>::new(self.x as f32, self.y as f32)
     }
 }
 
-impl Vecf64ToVecf32 for Vec3 {
-    fn convert(self) -> Vec3f32 {
-        Vec3f32::new(self.x as f32, self.y as f32, self.z as f32)
+impl Convert<3> for Vec3
+{
+    fn convert(&self) -> tv::VecD<3>
+    {
+        tv::VecD::<3>::new(self.x as f32, self.y as f32, self.z as f32)
     }
 }
 
+//{{{ collection: CurveViewMethod
+//{{{ enum: CurveViewMethod
 /// Options for generating points on a curve
 pub enum CurveViewMethod
 {
@@ -31,7 +46,8 @@ pub enum CurveViewMethod
     /// Points clustered in areas of high curvature
     Curvature,
 }
-
+//}}}
+//{{{ impl: Default for CurveViewMethod
 impl Default for CurveViewMethod
 {
     fn default() -> Self
@@ -40,23 +56,42 @@ impl Default for CurveViewMethod
     }
 }
 //..................................................................................................
-
-pub enum CurveColor
+//}}}
+//}}}
+//{{{ collection: CurveColor
+//{{{ enum:  CurveColor
+pub enum CurveColor<const D: usize>
 {
+    None,
     Solid(Color),
     ParamFunction(Box<dyn Fn(f64) -> f64>),
-    PositionFunction(Box<dyn Fn(Vec3) -> f64>),
+    PositionFunction(Box<dyn Fn(Vector<D>) -> f64>),
 }
-
-impl Default for CurveColor
+//}}}
+//{{{ impl : Default for CurveColor
+impl<const D: usize> Default for CurveColor<D>
 {
     fn default() -> Self
     {
         CurveColor::Solid(Color::default())
     }
 }
+//}}}
+//{{{ impl: Debug for CurveColor
+impl<const D: usize> Debug for CurveColor<D>
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            CurveColor::None => write!(f, "None"),
+            CurveColor::Solid(c) => write!(f, "Solid({:?})", c),
+            CurveColor::ParamFunction(_) => write!(f, "ParamFunction"),
+            CurveColor::PositionFunction(_) => write!(f, "PositionFunction"),
+        }
+    }
+}
+//}}}
 //..................................................................................................
-
+//}}}
 pub enum SurfaceColor
 {
     Solid(Color),
@@ -73,29 +108,12 @@ impl Default for SurfaceColor
 }
 //..................................................................................................
 
-/// Any type which implements this trait can be viewed in the viewer
-/// 
-/// Any implementation of this trait should provide a way to convert the type into a mesh 
-/// representation which for graphical viewing which can be rendered by the viewer
-pub trait Viewable2D {
-
-    /// This type, which must be implemented on a case-by-case basis for each type, provides the 
-    /// visualisation options for that type.
-    type Options;
-
-    /// This method converts the type into a mesh representation which can be rendered by the viewer
-    /// 
-    /// It does so by: 
-    /// - Creating a mesh representation of the object, there can be many such meshes
-    /// - Adding each mesh to the state object so that it can be rendered
-    fn view(&mut self, port: usize, opts: &Self::Options);
-}   
 
 /// Any type which implements this trait can be viewed in the viewer
 /// 
 /// Any implementation of this trait should provide a way to convert the type into a mesh 
 /// representation which for graphical viewing which can be rendered by the viewer
-pub trait Viewable3D {
+pub trait Viewable {
 
     /// This type, which must be implemented on a case-by-case basis for each type, provides the 
     /// visualisation options for that type.
