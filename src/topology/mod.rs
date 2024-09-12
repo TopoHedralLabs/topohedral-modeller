5df702de5226386a4d55eefc1e69675f217ab03b
John Ferguson <JAFerguson952@gmail.com>
2024-09-12 22:57:46 +0100

    created new 3d topology module under topology

--- src/topology/mod.rs
+++ src/topology/mod.rs
@@ -1,8 +1,2 @@
-//! This modules defines the set of topological entites in the modeller
-//!
-//!
-//--------------------------------------------------------------------------------------------------
 
-
-pub mod schema;
-pub mod bodies_regions_shells;
+pub mod d3;
